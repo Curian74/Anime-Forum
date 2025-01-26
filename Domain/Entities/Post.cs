8892bbbf57051479189f06d9fd87b6bdd9fd7398
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-01-26 13:03:36 +0700

    Modified Db Configs

--- Domain/Entities/Post.cs
+++ Domain/Entities/Post.cs
@@ -9,8 +9,8 @@ namespace Domain.Entities
     public class Post
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
     }
 }
