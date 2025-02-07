755d858d458bab1fc74c60be3e07c03914e98683
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-02-08 00:09:11 +0700

    Config and demo MVC project

---
+++ Domain/Common/Extensions/DeserializeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Extensions
+{
+    public static class DeserializeExtensions
+    {
+        private static JsonSerializerOptions defaultSettings = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true, //Case insensitive moi lay duoc dung properties trong json
+        };
+
+        //Goi thang nay moi khi can Deserialize
+        public static T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, defaultSettings)!; //Giong cai co san
+        }
+
+    }
+}
---
+++ WibuBlog/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Common.BaseEntities;
+using Domain.Common.Extensions;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace WibuBlog.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PostController(ApplicationDbContext dbContext, IHttpClientFactory httpClientFactory)
+        {
+            _dbContext = dbContext;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient("api"); //Lay client api tu program
+
+            var response = await client.GetAsync("Post/Get"); //endpoint build tu baseAddress
+
+            if(!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Ngu");
+            }
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+
+            //Goi extension custom
+            var wrapper = DeserializeExtensions.Deserialize<ApiResponse<Post>>(jsonResponse);
+
+            return View("Index", wrapper!.Value);
+        }
+    }
+}
