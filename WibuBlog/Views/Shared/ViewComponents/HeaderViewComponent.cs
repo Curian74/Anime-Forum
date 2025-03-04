9960402f884976cc048af98e381d830f258ebd63
QuocAnh1502 <anhcnqhe187233@fpt.edu.vn>
2025-03-04 10:05:10 +0700

    fixed header data & profile data

--- WibuBlog/Views/Shared/ViewComponents/HeaderViewComponent.cs
+++ WibuBlog/Views/Shared/ViewComponents/HeaderViewComponent.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WibuBlog.Services;
 
 namespace WibuBlog.Views.Shared.ViewComponents
 {
     //Dung class nay de gui data den Component tuong ung(Header)
-    public class HeaderViewComponent : ViewComponent
+    public class HeaderViewComponent(UserService userService) : ViewComponent
     {   
+        private readonly UserService _userService = userService;
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            //Data can gui se viet o day
-            return View();
+            var user = await _userService.GetUserProfile();
+            return View(user);
         }
     }
 }
